97c82a941779c928b1dc429dd1c078a1ebe58787
jv <jeff@jeffvier.com>
2013-10-18 15:34:15 -0700

    convert to v3 kibana

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,63 +3,20 @@ class kibana::install (
   $es_port        = 9200,
   $kibana_user    = 'daemon',
   $kibana_gid     = 'daemon',
-  $port           = '5601',
-  $bind_to        = '0.0.0.0',
-  $java_provider  = 'package',
-  $java_package   = 'java-1.7.0-openjdk',
+  $kibana_source  = 'https://github.com/elasticsearch/kibana.git',
 ) {
 
   Exec     { path => ["/usr/bin", "/bin", "/sbin"], }
   Package  { ensure => "installed", }
   File     { ensure => present, owner => root, group => root, mode => 0755, }
 
-  if $java_provider == 'package' {
-    if ! defined(Package[$java_package]) {
-      package { "$java_package": }
-    }
-  }
-
   exec {
     'git clone Kibana':
       cwd     => '/opt',
       user    => root,
-      command => "git clone https://github.com/rashidkpc/Kibana.git",
-      creates => "/opt/Kibana",
+      command => "git clone ${kibana_source}",
+      creates => "/opt/kibana",
       require => Package['git'];
-
-/*
-    'get jruby':
-      cwd     => '/opt',
-      command => 'wget http://jruby.org.s3.amazonaws.com/downloads/1.6.8/jruby-bin-1.6.8.tar.gz; tar -xzf jruby-bin-1.6.8.tar.gz',
-      creates => '/opt/jruby-1.6.8/bin';
-*/
-
-    'gem install bundler':
-      cwd     => '/opt',
-      path    => ['/bin', '/usr/bin'],
-      unless  => 'gem list | grep bundler',
-      require => [
-        Package['rubygems'],
-        Exec['git clone Kibana'],
-        ];
-
-    'Kibana bundle install':
-      cwd     => '/opt/Kibana',
-      path    => ['/bin', '/usr/bin'],
-      command => 'bundle install && touch Bundle.installedbypuppet',
-      creates => '/opt/Kibana/Bundle.installedbypuppet',
-      require => [Exec['gem install bundler'],];
-
-  }
-
-  file {
-    "/opt/Kibana/KibanaConfig.rb":
-      mode    => 644,
-      owner   => "$kibana_user",
-      group   => "$kibana_gid",
-      content => template("kibana/KibanaConfig.rb.erb"),
-      notify  => Daemontools::Service['kibana'],
-      require => Exec['git clone Kibana'];
   }
 
   daemontools::setup{
