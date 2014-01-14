2cdeceb16754d46fe0f297a51a56a913805829bf
jeff@jeffvier.com <jeff@jeffvier.com>
2014-01-13 16:37:47 -0800

    add a default dashboard and option to clobber it

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,6 @@
 class kibana::install (
-  $kibana_source  = 'https://github.com/elasticsearch/kibana.git',
+  $kibana_source   = 'https://github.com/elasticsearch/kibana.git',
+  $clobber_default = true,
 ) {
 
   Exec     { path => ["/usr/bin", "/bin", "/sbin"], }
@@ -13,5 +14,14 @@ class kibana::install (
       command => "git clone ${kibana_source}",
       creates => "/opt/kibana",
       require => Package['git'];
+  }
+
+  if ($clobber_default) {
+    file {
+      "/opt/kibana/src/app/dahboard/default.json":
+        content => template("${module_name}/default.json.erb"),
+        mode    => 0644,
+        require => Exec['git clone kibana'];
+    }
   }
 }
