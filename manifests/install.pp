8a29061395c88638c135f85d8c4ce21bef711060
jv <jeff@jeffvier.com>
2014-01-13 16:54:02 -0800

    add config.js.erb

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,7 @@
 class kibana::install (
   $kibana_source   = 'https://github.com/elasticsearch/kibana.git',
   $clobber_default = true,
+  $default_route   = '/dashboard/file/default.json',
 ) {
 
   Exec     { path => ["/usr/bin", "/bin", "/sbin"], }
@@ -14,6 +15,13 @@ class kibana::install (
       command => "git clone ${kibana_source}",
       creates => "/opt/kibana",
       require => Package['git'];
+  }
+
+  file {
+    "/opt/kibana/src/config.js":
+      content => template("${module_name}/config.js.erb"),
+      mode    => 0644,
+      require => Exec['git clone kibana'];
   }
 
   if ($clobber_default) {
